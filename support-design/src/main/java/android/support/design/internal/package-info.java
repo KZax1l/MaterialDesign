fd2fe5935b41914b5f39f44736ae5d3274285601
Zsago <3427241441@qq.com>
2017-04-08 23:15:55 +0800

    add support-design module that originate from design-25.3.1 of android support library.

---
+++ support-design/src/main/java/android/support/design/internal/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @hide
+ */
+@RestrictTo(LIBRARY_GROUP)
+package android.support.design.internal;
+
+import android.support.annotation.RestrictTo;
+
+import static android.support.annotation.RestrictTo.Scope.LIBRARY_GROUP;
